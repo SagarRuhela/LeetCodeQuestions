0658aee0c9779a5f3dc38d741f51a0d510e08042
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-05-11 11:50:35 +0530

    Time: 4 ms (99.50%), Space: 41.9 MB (83.87%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -1,51 +1,23 @@
 class Solution {
-    public static int Helper(int i,int j,int nums1[],int nums2[],int dp[][]){
-        if(i>=nums1.length || j>=nums2.length){
-            return 0;
-        }
-        int finalAns;
-        if(nums1[i]==nums2[j]){
-            int ans;
-            if(dp[i+1][j+1]!=-1){
-             ans=dp[i+1][j+1];
+    public static int helper(int nums1[],int nums2[]){
+        int n=nums1.length;
+        int m=nums2.length;
+        int dp[][]=new int[n+1][m+1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i][j+1],dp[i+1][j]);
+                }
             }
-             else{
-                 ans=Helper(i+1,j+1,nums1,nums2,dp);
-                 dp[i+1][j+1]=ans;
-               }
-               finalAns=ans+1;
             
         }
-        else{
-            int ans1;
-            int ans2;
-            if(dp[i][j+1]!=-1){
-                ans1=dp[i][j+1];
-            }
-            else{
-                ans1=Helper(i,j+1,nums1,nums2,dp);
-                dp[i][j+1]=ans1;
-            }
-            if(dp[i+1][j]!=-1){
-                ans2=dp[i+1][j];
-            }
-            else{
-                ans2=Helper(i+1,j,nums1,nums2,dp);
-                dp[i+1][j]=ans2;
-            }
-            finalAns=Math.max(ans1,ans2);
-            
-        }
-            return finalAns;
+        return dp[0][0];
     }
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int dp[][]=new int[nums1.length+1][nums2.length+1];
-        for(int i=0;i<nums1.length+1;i++){
-            for(int j=0;j<nums2.length+1;j++){
-                dp[i][j]=-1;
-            }
-        }
-        int ans=Helper(0,0,nums1,nums2,dp);
+        int ans=helper(nums1,nums2);
         return ans;
     }
 }
