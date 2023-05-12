65c60f7282f72e8872d785960f5df3ba2a3c8edf
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-05-12 20:51:25 +0530

    Added solution - LeetHub

---
+++ Array Operations - GFG/array-operations.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.arrayOperations(n, arr);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int arrayOperations(int n, int[] arr) {
+        // code here
+        int count=0;
+        boolean flag=false;
+        int start=0;
+        for(int i: arr){
+            if(i==0){
+                flag=true;
+                if(start>0){
+                    count++;
+                    start=0;
+                }
+            }
+            else{
+                start++;
+            }
+        }
+        if(flag && start>0){
+            count++;
+            return count;
+        }
+       else if(!(flag)){
+           return -1;
+       }
+       else{
+           return count;
+       }
+    }
+}
+        
