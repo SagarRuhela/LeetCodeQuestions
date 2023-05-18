b7df026ef0abde91c7ba792aa7b0c88f88ecb675
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-05-18 22:45:20 +0530

    Added solution - LeetHub

---
+++ Alternate positive and negative numbers - GFG/alternate-positive-and-negative-numbers.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().rearrange(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void rearrange(int arr[], int n) {
+        // code here
+        ArrayList<Integer> positiveNList = new ArrayList<>();
+        ArrayList<Integer> negativeNList = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
+                positiveNList.add(arr[i]);
+            }else{
+                negativeNList.add(arr[i]);
+            }
+        }
+        int i=0,j=0,k=0;
+        for(int l=0;l<n;l++){
+            if(i<positiveNList.size()){
+                arr[k++]=positiveNList.get(i++);
+            }
+            if(j<negativeNList.size()){
+                arr[k++]=negativeNList.get(j++);
+            }
+        }
+    }
+}
