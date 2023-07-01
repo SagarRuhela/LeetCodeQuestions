d42bc0ec9e40f402b1f320349c49ff19268dfa22
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-07-01 21:58:49 +0530

    Time: 453 ms (19.27%), Space: 40.2 MB (22.40%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,25 @@
+class Solution {
+    int ans;
+    int count[];
+    public int distributeCookies(int[] cookies, int k) {
+        ans=Integer.MAX_VALUE;
+         count=new int[k];
+        helper(cookies,0,k);
+        return ans;
+    }
+    public  void helper(int cookies[],int index,int k){
+        if(index==cookies.length){
+            int max=Integer.MIN_VALUE;
+            for(int i:count){
+                max=Math.max(max,i);
+            }
+            ans=Math.min(ans,max);
+            return;
+        }
+        for(int i=0;i<k;i++){
+        count[i]=count[i]+cookies[index];
+         helper(cookies,index+1,k);
+         count[i]=count[i]-cookies[index];   
+        }
+    }
+}
