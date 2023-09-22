6b62c552fcbc47602ddc711ee3641cb614c946aa
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-09-22 13:16:03 +0530

    Time: 5 ms (5.00%), Space: 42.8 MB (5.41%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,17 +1,36 @@
 class Solution {
  public boolean isSubsequence(String s, String t) {
-        return solve(s , 0 , t, 0);
+     int memo[][]=new int[s.length()+1][t.length()+1];
+     for(int i=0;i<memo.length;i++){
+         for(int j=0;j<memo[i].length;j++){
+             memo[i][j]=-1;
+         }
+     }
+     int finalans=solve(s , 0 , t, 0,memo);
+     if(finalans==s.length()){
+         return true;
+     }
+     else{
+         return false;
+     }
     }
     
-    private boolean solve(String s , int i , String t, int j){
-        if(i == s.length()){
-            return true;
+    private int solve(String s , int i , String t, int j,int memo[][]){
+        if(i>=s.length() || j>=t.length()){
+            return 0;
         }
-        if(i < s.length() && j == t.length()) return false;
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
         if(s.charAt(i) == t.charAt(j)){
-            return solve(s , i+1, t , j+1);
+            int ans=1+ solve(s,i+1,t,j+1,memo);
+            memo[i][j]=ans;
+            return ans;
         }else {
-            return solve(s , i, t, j+1);
+            int ans1= solve(s , i, t, j+1,memo);
+            int ans2=solve(s,i+1,t,j,memo);
+            memo[i][j]=Math.max(ans1,ans2);
+            return memo[i][j];
         }
         
     }
