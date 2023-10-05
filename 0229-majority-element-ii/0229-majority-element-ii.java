bed84a5e59d452ac54e0f37f62e1d821f890537e
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-10-05 18:02:41 +0530

    Time: 2 ms (98.52%), Space: 46.9 MB (30.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,24 +1,45 @@
-import java.util.Arrays;
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        Arrays.sort(nums);
-        int n=nums.length;
-        int count=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-            }
-            else {
-                if(count>n/3){
-                    list.add(nums[i-1]);
-                }
-                count=1;
-            }
-        }
-        if(count>n/3){
-            list.add(nums[n-1]);
-        }
-        return list;
-    }
+	List<Integer> result = new ArrayList<Integer>();
+	if (nums.length == 0)
+		return result;
+	// In the begining, both Trump and Biden don't get a vote
+	int firstMajor = Integer.MAX_VALUE, firstSum = 0, secondMajor = Integer.MIN_VALUE, secondSum = 0;
+	
+	for (int i = 0; i < nums.length; i++) {
+		// case 1: I want to vote for Biden or Trump
+		if (nums[i] == firstMajor)
+			firstSum++;
+		else if (nums[i] == secondMajor)
+			secondSum++;
+		// case 2: I want to run for the president
+		else if (firstSum == 0) {
+			firstMajor = nums[i];
+			firstSum = 1;
+		}
+		else if (secondSum == 0) {
+			secondMajor = nums[i];
+			secondSum = 1;
+		}
+		// case 3: fuck sleepy Joe and crazy Trump, let James be the president
+		else {
+			firstSum--;
+			secondSum--;
+		}
+	}
+	// After election, we need to count the vote again.
+	firstSum = 0;
+	secondSum = 0;
+	for (int i = 0; i < nums.length; i++) {
+		if (nums[i] == firstMajor)
+			firstSum++;    
+		else if (nums[i] == secondMajor)
+			secondSum++;
+	}
+	if (firstSum > nums.length / 3)
+		result.add(firstMajor);
+	if (secondSum > nums.length / 3)
+		result.add(secondMajor);
+	return result;
+}
 }
