41d496da1bfee233b551c02aed851b5eb51de3b2
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2023-11-19 13:54:08 +0530

    Time: 34 ms (81.58%), Space: 54.2 MB (80.26%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -1,23 +1,13 @@
 class Solution {
     public int reductionOperations(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i)+1);
-            }
-            else{
-                map.put(i,1);
-            }
-        }
         Arrays.sort(nums);
         int count=0;
-        for(int i=nums.length-1;i>=0;i--){
-            if(map.containsKey(nums[i])){
-           int step=map.size()-1;
-           step=step*map.get(nums[i]);
-           count=count+step;
-           map.remove(nums[i]);
+        int step=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                step++;
             }
+            count=count+step;
         }
         return count;
     }
