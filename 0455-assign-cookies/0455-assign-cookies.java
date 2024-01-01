911ed063f8750ed6493bcc49130b50a326ebf381
SagarRuhela <117362905+SagarRuhela@users.noreply.github.com>
2024-01-01 08:37:52 +0530

    Time: 11 ms (11.23%), Space: 45.4 MB (6.13%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<g.length && j< s.length){
+            if(g[i]<=s[j]){
+                count++;
+                j++;
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
